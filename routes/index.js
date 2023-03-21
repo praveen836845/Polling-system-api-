9f644c7b73a99f367177776282e8acab70d0af38
praveen836845 <77963498+praveen836845@users.noreply.github.com>
2023-03-20 22:31:46 -0700

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ const router = express.Router();
 console.log('Router_loaded');
 router.get('/' , (req,res) => {
     console.log("hello world");
-    res.status(200).send("<h1>Getting all question by readme file </h1>")
+    res.status(200).send("<h1>Getting all question by localhost:8000/api/questions </h1>")
 })
 router.use('/api', require('./api'));
 module.exports = router;
